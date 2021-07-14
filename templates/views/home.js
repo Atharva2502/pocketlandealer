19b62f19a089889b2986a67c88c41556929bdc7d
Atharva Nimbalkar <natharvanitin4@gmail.com>
2021-07-14 11:02:26 +0530

    Added

--- templates/views/home.js
+++ templates/views/home.js
@@ -6,10 +6,20 @@ $(window).on("load", function () {
             var objectBottom = $(this).offset().top + $(this).outerHeight();
 
             /* If the element is completely within bounds of the window, fade it in */
-            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-                if ($(this).css("opacity") == 0) { $(this).fadeTo(750, 1); }
-            } else { //object goes out of view (scrolling up)
-                if ($(this).css("opacity") == 1) { $(this).fadeTo(750, 0); }
+
+            if ($(window).width() > 1080) {
+                if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+                    if ($(this).css("opacity") == 0) { $(this).fadeTo(750, 1); }
+                } else { //object goes out of view (scrolling up)
+                    if ($(this).css("opacity") == 1) { $(this).fadeTo(750, 0); }
+                }
+            }
+            else {
+                if (objectBottom < windowBottom) { //object comes into view (scrolling down)
+                    if ($(this).css("opacity") == 0) { $(this).fadeTo(400, 1); }
+                } else { //object goes out of view (scrolling up)
+                    if ($(this).css("opacity") == 1) { $(this).fadeTo(400, 0); }
+                }
             }
         });
     }).scroll(); //invoke scroll-handler on page-load
