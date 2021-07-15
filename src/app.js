7e9834fe1aec52785086aa1f54c49faf85cc2d6b
Atharva Nimbalkar <natharvanitin4@gmail.com>
2021-07-15 22:47:26 +0530

    Added

--- src/app.js
+++ src/app.js
@@ -1047,12 +1047,12 @@ app.post("/send-request", requireLogin, async (req, res) => {
             <p style="text-align: center;font-size: 21px"><span style="padding-left: 17px;padding-right: 17px;padding-top: 8px;padding-bottom: 8px;color: white;background-color: black;border-radius: 2px;"><i style="font-weight: 200;">Mobile No</i> | ${buyer.userdata.mobNo}</span></p>
             <p style="text-align: center;font-size: 21px"><span style="padding-left: 17px;padding-right: 17px;padding-top: 8px;padding-bottom: 8px;color: white;background-color: black;border-radius: 2px;"><i style="font-weight: 200;">Email Address</i> | ${buyer.userdata.email}</span></p>
             <form action-xhr="https://pocketlandealer.herokuapp.com/send-docs" method="POST">
-            <div style="text-align: center;">
+            <div style="text-align: center; margin-top: 30px;">
             <input type="text" value="${buyer._id}" name="buyerid" style="display: none;">
-            <button type="submit" value="${seller._id}" name="send" style="text-decoration: none;color: white;font-size: 25px;background-color: black;padding: 15px;border-radius: 20px;font-family: Roboto;">Send Docs</button>
+            <button type="submit" value="${seller._id}" name="send" style="text-decoration: none;color: white;font-size: 25px;background-color: black;padding: 15px;border-radius: 20px;">Send Docs</button>
             </div>
             </form>
-            <p style="text-align: center;font-size: 18px;padding-top: 40px;">Please do contact on the above details as early as possible</p>
+            <p style="text-align: center;font-size: 18px;padding-top: 30px;">Please do contact on the above details as early as possible</p>
             <p style="text-align: center;font-size: 20px">Thank You !</p>
             <p style="text-align: center;font-size: 15px">@pocketlandealer.com</p>
             </div>
